5869e50af5deae52932d6f7ecddb72442e780278
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-02-26 22:42:14 +0700

    commit change

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,31 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Header</title>
-    <style>
-        body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
-        .header {
-            background-color: #f0f8ff; /* Light blue */
-            padding: 10px 0;
-            text-align: center;
-        }
-        .header a {
-            margin: 0 15px;
-            text-decoration: none;
-            color: #000; /* Black */
-        }
-        .header a:hover {
-            text-decoration: underline;
-            color: aqua;
-        }
-        .header .active {
-            color: red;
-        }
-    </style>
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+
+<body class="h-full">
+    <div class="min-h-full"></div></div>
