cabab038ce2d676cb5827574711c2c4f2306d7e6
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-02-28 00:54:37 +0700

    Commit changes

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
 include 'functions.php';
-include 'router.php';
+// include 'router.php';
+
+//Connect to MySQL
+$dsn = "mysql:host=localhost;port = 3306;dbname=myapp;user=root;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement ->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+
+// phpinfo();
